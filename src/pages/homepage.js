63d5d9b2f17242adf1969874d3e142c1a9cb510a
gustavoRuiz <jimgustavo1987@hotmail.com>
2018-12-29 19:49:56 -0500

    first commit

---
+++ src/pages/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import ProductListing from '../features/product-listing'
+
+export default function Homepage(props) {
+  return <div>
+    <h2>Homepage</h2>
+    <ProductListing />
+  </div>
+}
